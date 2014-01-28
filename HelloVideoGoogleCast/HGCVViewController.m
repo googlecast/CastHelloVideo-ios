7a490e917817ed5c5d207beb6ac790e46e1c7734
Kevin Nilson <kevin@javaclimber.com>
2014-01-28 15:13:34 -0800

    initial commit

---
+++ HelloVideoGoogleCast/HGCVViewController.m
@@ -0,0 +1,314 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "HGCVViewController.h"
+
+static NSString *const kReceiverAppID = @"YOUR_APP_ID_HERE";
+
+@interface HGCVViewController () {
+
+  UIImage *_btnImage;
+  UIImage *_btnImageSelected;
+}
+
+@property GCKMediaControlChannel *mediaControlChannel;
+@property GCKApplicationMetadata *applicationMetadata;
+@property GCKDevice *selectedDevice;
+@property(nonatomic, strong) GCKDeviceScanner *deviceScanner;
+@property(nonatomic, strong) UIButton *chromecastButton;
+@property(nonatomic, strong) GCKDeviceManager *deviceManager;
+@property(nonatomic, readonly) GCKMediaInformation *mediaInformation;
+
+@end
+
+@implementation HGCVViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  //Create chromecast button
+  _btnImage = [UIImage imageNamed:@"icon-cast-identified.png"];
+  _btnImageSelected = [UIImage imageNamed:@"icon-cast-connected.png"];
+
+  _chromecastButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  [_chromecastButton addTarget:self
+                        action:@selector(chooseDevice:)
+              forControlEvents:UIControlEventTouchDown];
+  _chromecastButton.frame = CGRectMake(0, 0, _btnImage.size.width, _btnImage.size.height);
+  [_chromecastButton setImage:nil forState:UIControlStateNormal];
+  _chromecastButton.hidden = YES;
+
+  self.navigationItem.rightBarButtonItem =
+      [[UIBarButtonItem alloc] initWithCustomView:_chromecastButton];
+
+  //Initialize device scanner
+  self.deviceScanner = [[GCKDeviceScanner alloc] init];
+
+  [self.deviceScanner addListener:self];
+  [self.deviceScanner startScan];
+
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+- (void)chooseDevice:(id)sender {
+  //Choose device
+  if (self.selectedDevice == nil) {
+    //Choose device
+    UIActionSheet *sheet =
+        [[UIActionSheet alloc] initWithTitle:NSLocalizedString(@"Connect to Device", nil)
+                                    delegate:self
+                           cancelButtonTitle:nil
+                      destructiveButtonTitle:nil
+                           otherButtonTitles:nil];
+
+    for (GCKDevice *device in self.deviceScanner.devices) {
+      [sheet addButtonWithTitle:device.friendlyName];
+    }
+
+    [sheet addButtonWithTitle:NSLocalizedString(@"Cancel", nil)];
+    sheet.cancelButtonIndex = sheet.numberOfButtons - 1;
+
+    //show device selection
+    [sheet showInView:_chromecastButton];
+  } else {
+    // Gather stats from device.
+    [self updateStatsFromDevice];
+
+    NSString *friendlyName = [NSString stringWithFormat:NSLocalizedString(@"Casting to %@", nil),
+        self.selectedDevice.friendlyName];
+    NSString *mediaTitle = [self.mediaInformation.metadata stringForKey:kGCKMetadataKeyTitle];
+
+    UIActionSheet *sheet = [[UIActionSheet alloc] init];
+    sheet.title = friendlyName;
+    sheet.delegate = self;
+    if (mediaTitle != nil) {
+      [sheet addButtonWithTitle:mediaTitle];
+    }
+
+    //Offer disconnect option
+    [sheet addButtonWithTitle:@"Disconnect"];
+    [sheet addButtonWithTitle:@"Cancel"];
+    sheet.destructiveButtonIndex = (mediaTitle != nil ? 1 : 0);
+    sheet.cancelButtonIndex = (mediaTitle != nil ? 2 : 1);
+
+    [sheet showInView:_chromecastButton];
+  }
+}
+
+- (void)updateStatsFromDevice {
+
+  if (self.mediaControlChannel && self.isConnected) {
+    _mediaInformation = self.mediaControlChannel.mediaStatus.mediaInformation;
+  }
+}
+
+- (BOOL)isConnected {
+  return self.deviceManager.isConnected;
+}
+
+- (void)connectToDevice {
+  if (self.selectedDevice == nil)
+    return;
+
+  NSDictionary *info = [[NSBundle mainBundle] infoDictionary];
+  self.deviceManager =
+      [[GCKDeviceManager alloc] initWithDevice:self.selectedDevice
+                             clientPackageName:[info objectForKey:@"CFBundleIdentifier"]];
+  self.deviceManager.delegate = self;
+  [self.deviceManager connect];
+
+}
+
+- (void)deviceDisconnected {
+  self.mediaControlChannel = nil;
+  self.deviceManager = nil;
+  self.selectedDevice = nil;
+}
+
+- (void)updateButtonStates {
+  if (self.deviceScanner.devices.count == 0) {
+    //Hide the cast button
+    _chromecastButton.hidden = YES;
+  } else {
+    //Show cast button
+    [_chromecastButton setImage:_btnImage forState:UIControlStateNormal];
+    _chromecastButton.hidden = NO;
+
+    if (self.deviceManager && self.deviceManager.isConnected) {
+      //Show cast button in enabled state
+      [_chromecastButton setTintColor:[UIColor blueColor]];
+    } else {
+      //Show cast button in disabled state
+      [_chromecastButton setTintColor:[UIColor grayColor]];
+
+    }
+  }
+
+}
+
+//Cast video
+- (IBAction)castVideo:(id)sender {
+  NSLog(@"Cast Video");
+
+  //Show alert if not connected
+  if (!self.deviceManager || !self.deviceManager.isConnected) {
+    UIAlertView *alert =
+        [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Not Connected", nil)
+                                   message:NSLocalizedString(@"Please connect to Cast device", nil)
+                                  delegate:nil
+                         cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                         otherButtonTitles:nil];
+    [alert show];
+    return;
+  }
+
+  //Define Media metadata
+  GCKMediaMetadata *metadata = [[GCKMediaMetadata alloc] init];
+
+  [metadata setString:@"Big Buck Bunny (2008)" forKey:kGCKMetadataKeyTitle];
+
+  [metadata setString:@"Big Buck Bunny tells the story of a giant rabbit with a heart bigger than "
+                       "himself. When one sunny day three rodents rudely harass him, something "
+                       "snaps... and the rabbit ain't no bunny anymore! In the typical cartoon "
+                       "tradition he prepares the nasty rodents a comical revenge."
+               forKey:kGCKMetadataKeySubtitle];
+
+  [metadata addImage:[[GCKImage alloc]
+      initWithURL:[[NSURL alloc] initWithString:@"http://commondatastorage.googleapis.com/"
+                                                 "gtv-videos-bucket/sample/images/BigBuckBunny.jpg"]
+            width:480
+           height:360]];
+
+  //define Media information
+  GCKMediaInformation *mediaInformation =
+      [[GCKMediaInformation alloc] initWithContentID:
+              @"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                                          streamType:GCKMediaStreamTypeNone
+                                         contentType:@"video/mp4"
+                                            metadata:metadata
+                                      streamDuration:0
+                                          customData:nil];
+
+  //cast video
+  [_mediaControlChannel loadMedia:mediaInformation autoplay:TRUE playPosition:0];
+
+}
+
+#pragma mark - GCKDeviceScannerListener
+- (void)deviceDidComeOnline:(GCKDevice *)device {
+  NSLog(@"device found!! %@", device.friendlyName);
+  [self updateButtonStates];
+}
+
+- (void)deviceDidGoOffline:(GCKDevice *)device {
+  [self updateButtonStates];
+}
+
+#pragma mark UIActionSheetDelegate
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+  if (self.selectedDevice == nil) {
+    if (buttonIndex < self.deviceScanner.devices.count) {
+      self.selectedDevice = self.deviceScanner.devices[buttonIndex];
+      NSLog(@"Selecting device:%@", self.selectedDevice.friendlyName);
+      [self connectToDevice];
+    }
+  } else {
+    if (buttonIndex == 1) {  //Disconnect button
+      NSLog(@"Disconnecting device:%@", self.selectedDevice.friendlyName);
+      // New way of doing things: We're not going to stop the applicaton. We're just going
+      // to leave it.
+      [self.deviceManager leaveApplication];
+      // If you want to force application to stop, uncomment below
+      //[self.deviceManager stopApplicationWithSessionID:self.applicationMetadata.sessionID];
+      [self.deviceManager disconnect];
+
+      [self deviceDisconnected];
+      [self updateButtonStates];
+    } else if (buttonIndex == 0) {
+      // Join the existing session.
+
+    }
+  }
+}
+
+#pragma mark - GCKDeviceManagerDelegate
+
+- (void)deviceManagerDidConnect:(GCKDeviceManager *)deviceManager {
+  NSLog(@"connected!!");
+
+  [self updateButtonStates];
+  [self.deviceManager launchApplication:kReceiverAppID];
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager
+    didConnectToCastApplication:(GCKApplicationMetadata *)applicationMetadata
+                      sessionID:(NSString *)sessionID
+            launchedApplication:(BOOL)launchedApplication {
+
+  NSLog(@"application has launched");
+  self.mediaControlChannel = [[GCKMediaControlChannel alloc] init];
+  self.mediaControlChannel.delegate = self;
+  [self.deviceManager addChannel:self.mediaControlChannel];
+  [self.mediaControlChannel requestStatus];
+
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager
+    didFailToLaunchCastApplicationWithError:(NSError *)error {
+  [self showError:error];
+
+  [self deviceDisconnected];
+  [self updateButtonStates];
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager
+    didFailToConnectWithError:(GCKError *)error {
+  [self showError:error];
+
+  [self deviceDisconnected];
+  [self updateButtonStates];
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager didDisconnectWithError:(GCKError *)error {
+  NSLog(@"Received notification that device disconnected");
+
+  if (error != nil) {
+    [self showError:error];
+  }
+
+  [self deviceDisconnected];
+  [self updateButtonStates];
+
+}
+
+- (void)deviceManager:(GCKDeviceManager *)deviceManager
+    didReceiveStatusForApplication:(GCKApplicationMetadata *)applicationMetadata {
+  self.applicationMetadata = applicationMetadata;
+}
+
+#pragma mark - misc
+- (void)showError:(NSError *)error {
+  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", nil)
+                                                  message:NSLocalizedString(error.description, nil)
+                                                 delegate:nil
+                                        cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                                        otherButtonTitles:nil];
+  [alert show];
+}
+
+@end
--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-CastHelloVideo-ios
-==================
+# hello-cast-video-ios
 
 This Hello Video demo application shows how an iOS sender application can cast a Video.  For simplicity this app is not fully compliant with the UX Checklist. 
+
+## Dependencies
+* iOS Sender API library : can be downloaded here at: [https://developers.google.com/cast/docs/downloads/](https://developers.google.com/cast/docs/downloads/ "iOS Sender API library")
+
+## Setup Instructions
+* Setup a Chromecast device
+* Regsiter an application on the Developers Console [http://cast.google.com/publish](http://cast.google.com/publish "Google Cast Developer Console"). The easisest would be to use the Styled Media Receiver option there. You will get an App ID when you finsih registering your application.
+* Setup the project dependencies in xCode
+* In ChromecastDeviceController.m, replace @"[YOUR\_APP\_ID_HERE]" with your app identifier from the Google Cast Developer Console. When you are done, it will look something like: 
+  * static NSString *const kReceiverAppID = @"1234ABCD";
+
+## Documentation
+Google Cast iOS Sender Overview:  [https://developers.google.com/cast/docs/ios_sender](https://developers.google.com/cast/docs/ios_sender "Google Cast iOS Sender Overview")
+
+## References and How to report bugs
+* Cast APIs: [https://developers.google.com/cast/](https://developers.google.com/cast/ "Google Cast Documentation")
+* Google Cast Design Checklist [http://developers.google.com/cast/docs/design_checklist](http://developers.google.com/cast/docs/design_checklist "Google Cast Design Checklist")
+* If you find any issues, please open a bug here on GitHub
+
+## How to make contributions?
+Please read and follow the steps in the [CONTRIBUTING.md](CONTRIBUTING.md)
+
+## License
+See [LICENSE](LICENSE)
